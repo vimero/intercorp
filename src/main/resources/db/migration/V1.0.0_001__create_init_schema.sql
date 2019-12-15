46e62f49be7aef36c8e975b15c326bc05c404b5d
Rony Villanueva <rony.villanueva@la-mark.com>
2019-12-15 15:41:49 -0500

    Database script

---
+++ src/main/resources/db/migration/V1.0.0_001__create_init_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `customer`(
+  `id` VARCHAR(36) PRIMARY KEY,
+  `first_name` VARCHAR(20) NOT NULL,
+  `last_name` VARCHAR(50) NOT NULL,
+  `age` SMALLINT UNSIGNED NOT NULL,
+  `date_birthday` datetime NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
