eeea0bf6a5dea29b8eaf4d121d71b6a25b03de79
Rony Villanueva <rony.villanueva@la-mark.com>
2019-12-15 15:52:25 -0500

    Application boot

---
+++ src/main/java/io/vimero/intercorp/IntercorpApplication.java
@@ -0,0 +1,18 @@
+package io.vimero.intercorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Rony Villanueva <rony@vimero.io>
+ * @since 15/12/2019
+ */
+
+@SpringBootApplication
+public class IntercorpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntercorpApplication.class, args);
+    }
+
+}
