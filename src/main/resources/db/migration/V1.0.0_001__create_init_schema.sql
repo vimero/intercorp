f0c316d5b736f969ca088dac7d373facbac19b25
Rony Villanueva <rony.villanueva@la-mark.com>
2019-12-16 00:47:02 -0500

    Add date death

--- src/main/resources/db/migration/V1.0.0_001__create_init_schema.sql
+++ src/main/resources/db/migration/V1.0.0_001__create_init_schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE `customer`(
   `last_name` VARCHAR(50) NOT NULL,
   `age` SMALLINT UNSIGNED NOT NULL,
   `date_birthday` datetime NOT NULL,
+  `date_death` datetime NOT NULL,
   `date_created` datetime NOT NULL,
   `date_updated` datetime
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
